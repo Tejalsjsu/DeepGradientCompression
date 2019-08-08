de04cc643ec46cb20f0f9b27c520f008b5271976
Laxmidas <padhteja@8c85909760c1.ant.amazon.com>
2019-08-08 11:32:43 -0700

    script to run MNIST on 1 node multiple gpu

---
+++ examples/MNIST/scripts/run_mnist_estimator.sh
@@ -0,0 +1,21 @@
+startTime=${SECONDS}
+
+sudo rm -r mnist_convnet_model
+source activate tensorflow_p36
+
+
+mpi_command="/home/ubuntu/anaconda3/envs/tensorflow_p36/bin/mpirun -np 16 -H localhost:8,172.31.6.212:8  \
+--allow-run-as-root -bind-to none -map-by slot \
+-x NCCL_DEBUG=INFO \
+-x LD_LIBRARY_PATH \
+-x NCCL_SOCKET_IFNAME=ens5 -mca btl_tcp_if_exclude lo,docker0 \
+-x PATH -mca pml ob1 -mca btl ^openib"
+use_hvd="--horovod"
+
+
+$mpi_command python mnist_estimator.py
+
+
+endTime=${SECONDS}
+diffTime=`expr ${endTime} - ${startTime}`
+echo "Diff Time: [${diffTime}]":
